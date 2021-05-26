960263cf964073f36df7de0b9f58d92fd8b86d20
Dhanashree <dhanashreesarade919@gmail.com>
2021-05-26 23:35:06 +0530

    adddddd

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,18 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
-  const [input,setInput] = useState();
+  const [input,setInput] = useState(0);
 
   const inputting=(event)=>{
     setInput(event.target.value)
-  
-
   }
-
+  
     React.useEffect(() => {
+
       const timer =
-     Number(input > 0 && setInterval(() => setInput(input - 1), 1000))
-     return () => clearInterval(timer)
-      
-    }, [input]);
+      Number(input > 0 && setInterval(() => setInput(input - 1), 1000))
+       return () => clearInterval(timer)
+      }, [input]);
 
 
 
@@ -26,8 +24,10 @@ const App = () => {
         <h1>
           Reverse countdown for<input id="timeCount" onKeyDown={inputting} /> sec.
         </h1>
-      </div>
-         <div id="current-time">{input}</div>
+      </div>{
+        input == '0' || input == "" || input<=0 || isNaN(input) ? "0" :  <div id="current-time">{input}</div>
+      }
+        
     </div>
   )
 }
